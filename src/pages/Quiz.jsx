b5a7cf46445cb1dada0bc76cdce6234715c922de
GodWilldev <godwillmbdev@gmail.com>
2022-10-27 21:56:12 +0200

    update in Quiz component

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -13,13 +13,15 @@ function Quiz() {
 
   return (
     <div>
-      <div className='text-center mb-5'>
-        <p className='h2'>{questions[currentQuestion].type} Quiz</p>
-      </div>
-      {currentQuestion+1<5 && 
+      {currentQuestion<5 &&
+      <>
+        <div className='text-center mb-5'>
+          <p className='h2'>{questions[currentQuestion].type} Quiz</p>
+        </div>
         <Question question={questions[currentQuestion]}/>
+      </>
       }
-      {currentQuestion+1>=5 && 
+      {currentQuestion>=5 && 
         <Results rate={rate} />
       }
     </div>
